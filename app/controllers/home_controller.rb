3068f0b7aea81e5699036dfd0674fc1515527d53
Hajime Furukawa <hajpoj@gmail.com>
2012-01-14 16:38:51 -0800

    Updated css/html of homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < SongRakeController
   skip_before_filter :authenticate_admin, :only => [:index, :about]
 
   def index
+    @playlists = Playlist.where(:playlist_type => "Listed").limit(5)
   end
 
   def about
