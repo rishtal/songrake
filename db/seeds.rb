f19ef53d60f77fbcb5ce451c254327544cc8ddf5
Hajime Furukawa <hajpoj@gmail.com>
2012-02-07 00:00:21 -0800

    update seeds file to have different seeds for production and development

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,62 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'factory_girl'
 
-@user_djwronger = FactoryGirl.create(:user, :username => "djwronger", :email => "djwronger@songrake.com")
+case Rails.env
 
-@user_jane = FactoryGirl.create(:user, :username => "Jane Tan", :email => "janetan@songrake.com")
-@user_mike = FactoryGirl.create(:user, :username => "Michael", :email => "michael@songrake.com")
-@user_taylor =FactoryGirl.create(:user, :username => "Taylor", :email => "taylor@songrake.com")
+  when "development"
 
-@micronics_party = FactoryGirl.create(:playlist, :name => "Micronics Holiday Party")
+    @user_djwronger = FactoryGirl.create(:user, :username => "djwronger", :email => "djwronger@songrake.com", :role => "User")
 
-FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_djwronger, :role => "Creator")
-FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_jane)
-FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_mike)
-FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_taylor)
+    @user_jane = FactoryGirl.create(:user, :username => "Jane Tan", :email => "janetan@songrake.com")
+    @user_mike = FactoryGirl.create(:user, :username => "Michael", :email => "michael@songrake.com")
+    @user_taylor =FactoryGirl.create(:user, :username => "Taylor", :email => "taylor@songrake.com")
 
-FactoryGirl.create(:song,
-                   :title => "Can't touch this",
-                   :artist => "MC Hammer",
-                   :playlist => @micronics_party,
-                   :requester => @user_jane)
+    @micronics_party = FactoryGirl.create(:playlist, :name => "Micronics Holiday Party")
 
-FactoryGirl.create(:song,
-                   :title => "You Da One",
-                   :artist => "Rihanna",
-                   :playlist => @micronics_party,
-                   :requester => @user_mike)
+    FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_djwronger, :role => "Creator")
+    FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_jane)
+    FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_mike)
+    FactoryGirl.create(:playlist_role, :playlist => @micronics_party, :user => @user_taylor)
 
-FactoryGirl.create(:song,
-                   :title => "Never going to give you up",
-                   :artist => "Rick Astley",
-                   :playlist => @micronics_party,
-                   :requester => @user_taylor)
+    FactoryGirl.create(:song,
+                       :title => "Can't touch this",
+                       :artist => "MC Hammer",
+                       :playlist => @micronics_party,
+                       :requester => @user_jane)
 
-FactoryGirl.create(:song,
-                   :title => "You Belong with me",
-                   :artist => "Taylor Swift",
-                   :playlist => @micronics_party,
-                   :requester => @user_taylor)
+    FactoryGirl.create(:song,
+                       :title => "You Da One",
+                       :artist => "Rihanna",
+                       :playlist => @micronics_party,
+                       :requester => @user_mike)
 
-FactoryGirl.create(:song,
-                   :title => "Levels",
-                   :artist => "AVICII",
-                   :playlist => @micronics_party,
-                   :requester => @user_djwronger)
+    FactoryGirl.create(:song,
+                       :title => "Never going to give you up",
+                       :artist => "Rick Astley",
+                       :playlist => @micronics_party,
+                       :requester => @user_taylor)
 
-FactoryGirl.create(:song,
-                   :title => "Help",
-                   :artist => "Beatles",
-                   :playlist => @micronics_party,
-                   :requester => @user_djwronger)
+    FactoryGirl.create(:song,
+                       :title => "You Belong with me",
+                       :artist => "Taylor Swift",
+                       :playlist => @micronics_party,
+                       :requester => @user_taylor)
+
+    FactoryGirl.create(:song,
+                       :title => "Levels",
+                       :artist => "AVICII",
+                       :playlist => @micronics_party,
+                       :requester => @user_djwronger)
+
+    FactoryGirl.create(:song,
+                       :title => "Help",
+                       :artist => "Beatles",
+                       :playlist => @micronics_party,
+                       :requester => @user_djwronger)
+
+  when "production"
+
+    @user_djwronger = FactoryGirl.create(:user, :username => "djwronger", :email => "djwronger@songrake.com", :role => "User")
+
+end
+
